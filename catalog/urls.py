6a5fe3a985eb9436fa0499e2018f72623d6556a9
Hapikc <136709525532hs@gmail.com>
2024-12-03 13:57:07 +0700

    6-module almost done

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import re_path
 from . import views
 
 
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    re_path('', views.index, name='index'),
+    re_path(r'^books/$', views.BookListView.as_view(), name='books'),
+    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 
 def index(request):
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
     num_genre = Genre.objects.all().count()
-    num_books_title = Book.objects.filter(title='Первая стена').count()
+    num_books_title = Book.objects.filter(title__icontains='Заблудшие').count()
 
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
@@ -18,3 +19,14 @@ def index(request):
                  'num_books_title': num_books_title,
                  'num_instances_available': num_instances_available, 'num_authors': num_authors},
     )
+
+
+class BookListView(generic.ListView):
+    model = Book
+
+    context_object_name = 'my_book_list'
+    queryset = Book.objects.filter(title__icontains='Заблудшие')[:5]
+    template_name = 'books/book_list.html'
+
+class BookDetailView(generic.DetailView):
+    model = Book
